691706dd32271df7dbdc2503239f8a17d5e69afe
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2025-09-29 17:10:52 +0700

    feat(utils/images): restructure image paths and add team member images

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,9 +1,12 @@
-import logo from '~/assets/images/logo.jpg';
-import logo_remove_bg from '~/assets/images/logo-remove-bg.png';
-import poster from '~/assets/images/poster_black_white.jpg';
+import logo from '~/assets/images/Logo/logo.jpg';
+import logo_remove_bg from '~/assets/images/Logo/logo-remove-bg.png';
+import poster from '~/assets/images/Poster/poster_black_white.jpg';
+import bg_studio from '~/assets/images/Background/bg-studio.png';
 import img_1 from '~/assets/images/img-1.jpg';
 import img_2 from '~/assets/images/img-2.jpg';
 import img_3 from '~/assets/images/img-3.jpg';
-import bg_studio from '~/assets/images/bg-studio.png';
+import member_1 from '~/assets/images/TeamMember/member_1.png';
+import member_2 from '~/assets/images/TeamMember/member_2.png';
+import member_3 from '~/assets/images/TeamMember/member_3.png';
 
-export { logo, logo_remove_bg, poster, img_1, img_2, img_3, bg_studio };
+export { logo, logo_remove_bg, poster, img_1, img_2, img_3, bg_studio, member_1, member_2, member_3 };
