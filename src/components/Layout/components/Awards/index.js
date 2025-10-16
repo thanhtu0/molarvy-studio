69fdb7cf148a9c2a2c3326cfc968e497956dc9ee
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2025-10-16 10:01:22 +0700

    refactor: Separate data from components

--- src/components/Layout/components/Awards/index.js
+++ src/components/Layout/components/Awards/index.js
@@ -1,19 +1,9 @@
 import classNames from 'classnames/bind';
 import styles from './Award.module.scss';
 import Section from '~/components/common/Section';
+import { awardsList } from '~/data/awardsData';
 
 const cx = classNames.bind(styles);
-
-const awardsList = [
-    '"Pocket Gamer Awards Best Narrative Game 2021 (Nominee)" - FAR: Lone Sails',
-    '"Pocket Gamer Awards Best Mobile Conversion 2021 (Nominee)" - FAR: Lone Sails',
-    '"TapTap Award Best Indie Game 2020 (Winner)" - FAR: Lone Sails',
-    '"Reboot Develop Blue Indie Award Game of the Year 2019 (Winner)" - FAR: Lone Sails',
-    '"Deutscher Computerspielpreis Best International Game 2019 (Nominee)" - FAR: Lone Sails',
-    '"Swiss Game Awards Main Prize 2019 (Winner)" - FAR: Lone Sails',
-    '"Game Developers Choice Awards Best Debut 2019 (Honorable Mention)" - FAR: Lone Sails',
-    '"Golden Joystick Awards Best Visual Design 2018 (Nominee)" - FAR: Lone Sails',
-];
 
 function Awards() {
     return (
